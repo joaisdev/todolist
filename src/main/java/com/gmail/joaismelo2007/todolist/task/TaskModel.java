ddc96ebd34f35fbb86c03cb55ff24d95562e4842
joaisdev <joaismelo2007@gmail.com>
2026-03-26 00:31:44 -0300

    Docker file deploy

---
+++ src/main/java/com/gmail/joaismelo2007/todolist/task/TaskModel.java
@@ -0,0 +1,54 @@
+package com.gmail.joaismelo2007.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+ /**
+     * 
+     * ID
+     * Usuário (ID_USUARIO)
+     * Descrição
+     * Título
+     * Data de início
+     * data de término
+     * Propriedade
+     * 
+     */
+
+ @Data
+ @Entity(name = "tb_tasks")
+public class TaskModel {
+
+   
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+    private String priority;
+    
+    @Column(length = 50)
+    private String title;
+
+    private LocalDateTime startAt, endAt;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt; 
+
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50){
+            throw new Exception("o campo title deve conter no máximo 50 caracteres");
+        }
+
+        this.title = title;
+    }
+}
